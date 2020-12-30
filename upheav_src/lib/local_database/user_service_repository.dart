2d424e9b729ae4912e5ef9ad0337fcfb9ac60e85
Hasib Ullah <hasib.ascend@gmail.com>
2020-12-30 23:49:41 +0600

    User add, update, delete repository functions created

--- upheav_src/lib/local_database/user_service_repository.dart
+++ upheav_src/lib/local_database/user_service_repository.dart
@@ -33,4 +33,76 @@ class UserServiceRepository {
     final user = UserModel.fromJson(data.first);
     return user;
   }
+
+  // Adds a new user to the user Database
+  static Future<void> addUser(UserModel user) async {
+    /*final sql = '''INSERT INTO ${DatabaseCreator.todoTable}
+    (
+      ${DatabaseCreator.id},
+      ${DatabaseCreator.name},
+      ${DatabaseCreator.info},
+      ${DatabaseCreator.isDeleted}
+    )
+    VALUES 
+    (
+      ${todo.id},
+      "${todo.name}",
+      "${todo.info}",
+      ${todo.isDeleted ? 1 : 0}
+    )''';*/
+
+    final sql = '''INSERT INTO ${DatabaseCreator.userTable}
+    (
+      ${DatabaseCreator.id},
+      ${DatabaseCreator.name},
+      ${DatabaseCreator.isDeleted}
+    )
+    VALUES (?,?,?)''';
+    List<dynamic> params = [user.id, user.name, user.isDeleted ? 1 : 0];
+    final result = await db.rawInsert(sql, params);
+    DatabaseCreator.databaseLog('Add user', sql, null, result, params);
+  }
+
+  static Future<void> deleteUser(UserModel user) async {
+    /*final sql = '''UPDATE ${DatabaseCreator.todoTable}
+    SET ${DatabaseCreator.isDeleted} = 1
+    WHERE ${DatabaseCreator.id} = ${todo.id}
+    ''';*/
+
+    final sql = '''UPDATE ${DatabaseCreator.userTable}
+    SET ${DatabaseCreator.isDeleted} = 1
+    WHERE ${DatabaseCreator.id} = ?
+    ''';
+
+    List<dynamic> params = [user.id];
+    final result = await db.rawUpdate(sql, params);
+
+    DatabaseCreator.databaseLog('Delete user', sql, null, result, params);
+  }
+
+  static Future<void> updateTodo(UserModel user) async {
+    /*final sql = '''UPDATE ${DatabaseCreator.todoTable}
+    SET ${DatabaseCreator.name} = "${todo.name}"
+    WHERE ${DatabaseCreator.id} = ${todo.id}
+    ''';*/
+
+    final sql = '''UPDATE ${DatabaseCreator.userTable}
+    SET ${DatabaseCreator.name} = ?
+    WHERE ${DatabaseCreator.id} = ?
+    ''';
+
+    List<dynamic> params = [user.name, user.id];
+    final result = await db.rawUpdate(sql, params);
+
+    DatabaseCreator.databaseLog('Update user', sql, null, result, params);
+  }
+
+  static Future<int> usersCount() async {
+    final data = await db
+        .rawQuery('''SELECT COUNT(*) FROM ${DatabaseCreator.userTable}''');
+
+    int count = data[0].values.elementAt(0);
+    int idForNewItem = count++;
+    return idForNewItem;
+  }
 }
